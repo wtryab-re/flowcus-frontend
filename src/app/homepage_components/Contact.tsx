7e74f6acd1c4b9438be3d519b41583052f1416b1
wtryab <wtryab@gmail.com>
2026-07-30 03:51:20 +0500

    v1

--- src/app/homepage_components/Contact.tsx
+++ src/app/homepage_components/Contact.tsx
@@ -11,12 +11,12 @@ export default function Contact() {
     message: "",
   });
 
-  const handleChange = (e) => {
+  const handleChange = (e: any) => {
     const { name, value } = e.target;
     setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: any) => {
     e.preventDefault();
     // Handle form submission logic here
     console.log("Submitted Feedback/Inquiry:", formData);
